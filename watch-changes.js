ad4fdcce08bef3bc523c27c5714f52cfe30ff791
Hielke <hielkemaps@gmail.com>
2024-06-15 18:15:36 +0800

    Update watch-changes.js

--- watch-changes.js
+++ watch-changes.js
@@ -2,7 +2,9 @@ const chokidar = require('chokidar');
 const fs = require('fs-extra');
 
 const sourcePath = 'TreeCapitator';
-const datapacksFolder = 'C:\\Users\\Hielk\\AppData\\Roaming\\.modrinth\\profiles\\Build\\saves\\TreeCapitator\\datapacks';
+
+// Change this to the datapacks folder of your world!
+const datapacksFolder = 'C:\\Users\\Hielk\\AppData\\Roaming\\.minecraft\\saves\\TreeCapitator\\datapacks';
 
 const watcher = chokidar.watch(sourcePath, {
   ignored: /(^|[/\\])\../, // Ignore hidden files
